0d102b6134c3094a430edb987f7a068778f21018
Rui Liu <lriuui0x0@gmail.com>
2022-06-06 12:50:43 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+/*==================== MENU SHOW/ HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu')
+const navClose = document.getElementById("nav-close")
+const navToggle = document.getElementById('nav-toggle')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', ()=> {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', ()=> {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+navLink.forEach(link => link.addEventListener('click',()=> {
+        navMenu.classList.remove('show-menu')
+    })
+)
+
+
+/*==================== ACCORDION SKILLS ====================*/
+
+
+/*==================== QUALIFICATION TABS ====================*/
+
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
